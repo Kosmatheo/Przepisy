e5a5fb75ab13eb21db325fbdf2561191da5f1b90
Kosmatheo <Kosmafzar@gmail.com>
2025-11-28 09:02:13 +0100

    Dodanie przepisow

---
+++ app/src/main/java/com/example/kosmapokosciach/RepozytoriumPrzepisow.java
@@ -0,0 +1,21 @@
+package com.example.kosmapokosciach;
+
+import java.util.ArrayList;
+
+public class RepozytoriumPrzepisow {
+    private static ArrayList<Przepis> przepisy;
+    private static void wygenerujPrzepisy(){
+        przepisy = new ArrayList<>();
+        przepisy.add(new Przepis("ciastka z borówkami" , "ciasteczka",R.drawable.ciastka_z_b, "jaja , czekolada , borówki" , "opis"));
+        przepisy.add(new Przepis("ciastka francuskie" , "ciasteczka",R.drawable.ciastka_francuskie, "jaja , czekolada , borówki" , "opis"));
+        przepisy.add(new Przepis("ciastka" , "ciasteczka",R.drawable.ciastkajpg, "jaja , czekolada , borówki" , "opis"));
+        przepisy.add(new Przepis("deser z truskawkami" , "ciasteczka",R.drawable.deser_t, "jaja , czekolada , borówki" , "opis"));
+        przepisy.add(new Przepis("sernik" , "ciasto",R.drawable.sernik, "jaja , czekolada , borówki" , "opis"));
+        przepisy.add(new Przepis("kakao" , "napoj",R.drawable.kakao, "jaja , czekolada , borówki" , "opis"));
+        przepisy.add(new Przepis("deser z truskawkami" , "ciasteczka",R.drawable.deser_t, "jaja , czekolada , borówki" , "opis"));
+    }
+    public static ArrayList<Przepis> zwrocPrzepisy(){
+        return przepisy;
+    }
+
+}
